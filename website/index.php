f306707aae0514f93f8b9ee0e681ccff23f5d31f
Ukhaey <114957146+Ukhaey@users.noreply.github.com>
2025-10-17 13:57:27 +0100

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav >
+<ul style="display: flex;color:rgb(34, 3, 3);background-color: white; justify-content: space-evenly;">
+<a href=""> <b>Kagont!</b></a>
+<a href="">Home</a>
+<a href="ukhaey.html">Shop</a>
+<a href="">Single Products</a>
+<a href="ukay.html">About</a>
+<a href="ukah.html">Contact</a>
+</ul>
+    </nav>
+
+    <img src="De shop pics/Garnier Banner.png_F5yB857wU_.webp" style="width:90rem;">
+<section>
+    <ul style="display: flex;justify-content: space-evenly;font-size: xx-large;">
+<p> <b>Begarto</b> </p>
+<p> <b>EUgEN</b></p>
+<p>AEROMIZ</p>
+<p> <b>Macery</b></p>
+<p>DiegoMares</p>
+    </ul>
+</section>
+
+<section style="background-image: url(Nivea\ Banner\ 650.png_uh9jHpSLTW.webp);width: 90rem;height: 20rem;padding-top:90px">
+    <p style="color: white; text-align: center;font-size: xx-large;font-weight: bolder; "> <b>FEATURED PRODUCTS</b></p>
+  <div style="display: flex;justify-content: center;gap: 15px;">
+    <div>
+ <img src="pic-12-2.jpg" style="width:10cm;">
+    <p style="font-size:large"><strong>Knots your Long Sleeve</strong> </p>
+    <p style="color: tomato;"> <b>$75.00</b></p>
+    <button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button>
+</div>
+
+<div>
+<img src="pic-9-2.jpg" alt="" style="width: 10cm;">
+<p style="font-size: larger;"> <b>Evolve Antres Activewears</b></p>
+<p style="color: tomato;">  <strike>$18.00</strike><b> $9.00 <b></p>
+<button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button>
+</div>
+<div>
+    <img src="pic-7-2.jpg" alt="" style="width: 10cm;">
+    <p style="font-size: larger;">Simply Easy Peasy Tank</p>
+    <p style="color: tomato;"><b>$34.99 <b></p>
+    <button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button>
+</div>
+</div>
+<section style="display: flex;justify-content: center;gap: 30px;margin-top: 150px;">
+    <div>
+<img src="De shop pics/Ramadan Special (1).gif_fZ9GC9qbSE.webp" alt="">
+</div>
+
+<div>
+    <img src="De shop pics/Ramadan Special (3).gif_Dx3G1vs1Xn.webp" alt="">
+</div>
+</section>
+
+
+   <h1 style="text-align: center;margin-top: 100px;font-weight: 600px;"> <b>SALES PRODUCTS</b></h1>
+<section style="display: flex;justify-content: center; margin-top: 20px;gap: 20px;">
+<div>
+<img src="pic-27.jpg" alt="" style="width: 10cm;">
+<p>Laney Ribbed Maxi Dress</p>
+<p style="color: tomato;"> <strike>110.00</strike> <u>75.00</u></p>
+<button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button></button>
+</div>
+<div>
+<img src="pic-26 (1).jpg" alt="" style="width: 10cm;">
+<p>Angelic Moments Mini Dress</p>
+<p style="color: tomato;"> <strike>190.00</strike> <u>$110.00</u></p>
+<button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button></button>
+
+</div>
+
+<div>
+    <img src="pic-24.jpg" alt="" style="width: 10cm;">
+    <p>Better With Me Mini Dress</p>
+    <p style="color: tomato;"><strike>$75.00</strike> <u>$55.00</u></p>
+    <button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button></button>
+</div>
+</section>
+
+<section style="display: flex;justify-content: center;gap: 20px; margin-top: 30px;">
+    <div>
+<img src="pic-22-2.jpg" alt="" style="width: 10cm;">
+<p>Chelsea Leggings Set</p>
+<p style="color: tomato;"><strike>$310.00</strike> <u>220.00</u></p>
+<button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button></button>
+</div>
+
+<div>
+    <img src="pic-20-2.jpg" alt="" style="width: 10cm;">
+    <p>Eternal welcome To Paradise</p>
+    <p style="color: tomato;"><strike>$110.00</strike> <u>$90.00</u></p>
+    <button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Buy Products</button>
+
+</div>
+
+<div>
+    <img src="pic-9-2.jpg" alt="" style="width: 10cm;">
+    <p>Evolve Anteres Wears</p>
+    <p style="color: tomato;"><strike>$180.00</strike> <u>$90.00</u></p>
+    <button style="color: black;background-color:white;border-color: tomato;padding-left: 40px;padding-right: 40px;padding-top: 10px;padding-bottom: 20px;">Add to Cart</button></button>
+</div>
+</section>
+
+<section>
+<img src="De shop pics/BRKTech_1.gif_q2qpdDiQWi.webp" alt="" style="width: 90rem;margin-top:100px ;">
+<ul style="display: flex; justify-content: center; gap:100px;margin-top: 100px;">
+<div>
+<h1> <b>Free Shopping</b></h1>
+<p style="font-weight: lighter;">Order Over $500</p>
+</div>
+<div>
+<h1><b>Quick Payment</b></h1>
+<p style="font-weight: lighter;">100% Secure Payment</p>
+</div>
+<div>
+<h1><b>24/7 Support</b></h1>
+ <p style="font-weight: lighter;">Ready For You </p>
+</div>
+<div>
+<h1>Money Return</h1>
+<p style="font-weight: lighter;">Back Under 7 Days </p>
+</div>
+
+</ul>
+</section>
+
+
+<section style="margin-top: 80px;padding-left:300px;padding-right:200px;margin-bottom: 50px;">
+    <div style=" background-color: rgb(11, 54, 2);color: white; text-align: center; margin-bottom: 100px; padding-left: 100px;padding-right: 100px;padding-top: 50px;">
+    
+    <h1><b>GET DISCOUNT INFO</b></h1>
+    <P style="font-weight: lighter;margin-bottom: 50px;">Detract yet delight written farther his general.If in so bred at dare rose lose good.
+         <br>Feel and make two real miss use easy.Celebrated delightful an especially increasing.</P>
+         <input type="text" placeholder="sample@gmail.com" style="background-color: white;padding-left:10px;padding-right: 400px;padding-bottom: 10px;padding-top:10px;margin-bottom: 50px;">
+</div>
+</section>
+
+<section style="display:flex;justify-content:center; gap:100px;background-color: black;color: white;padding-bottom: 100px;">
+<div>
+<h1 style="color: white;"><a href="duff.html">About Us</a></h1>
+<p style="font-size: small;color: white;">215,Woodlane Ave, Manchester, NH03102</p>
+<P style="font-size: small;color: white;">Sun-Sat:9:00AM-17:00PM</P>
+<p style="font-size: small;color: white;">(708)856-0657</p>
+
+</div>
+<div>
+    <h1 style="color: white;">Get Help</h1>
+    <p style="font-size: small;color: white;">help center </p>
+    <p style="font-size: small;color: white;">Shipping Info </p>
+    <p style="font-size: small;color: white;">Returns</p>
+</div>
+<div>
+<h1 style="color: white;">Company</h1>
+<P style="font-size: small;color: white;">Careers</P>
+<p style="font-size: small;color: white;">About</p>
+<p style="font-size: small;color: white;">Stores</p>
+<p style="font-size: small;color: white;">Want to Collab?</p>
+</div>
+
+ <div>
+<h1 style="color: white;">Quick Lines</h1>
+<p style="font-size: small;color: white;">Size Guide</p>
+<p style="font-size: small;color: white;">Gift Card</p>
+<p style="font-size: small;color: white;">Check  Gift Card Balance</p>
+<p style="font-size: small;color: white;">Contact Us</p>
+ </div>
+</section>
+</body>
+</html>
